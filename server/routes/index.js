292b9d3b47aec2f3a3968e02e0115b397bf583c2
KnarManukyan <Knarik_Manukyan@edu.aua.am>
2018-08-02 08:49:05 +0400

    setup fixed

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,4 @@
 var express = require('express');
-var server = require('../server.js');
-var database = require('../repository/repository.js');
 var router = express.Router();
 
 console.log("Hello from the back-end siiiiiiiide! At least I can say that I've tried!");
--- server/server.js
+++ server/server.js
@@ -2,13 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+const router = require('./routes/index.js')
 
-app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(cors());
